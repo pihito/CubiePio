7185f9c17312e79bc757a6b22665a91a36c98799
sebastien D <sdelporte@gmail.com>
2014-04-17 17:26:34 +0200

    renew pio work

---
+++ build/CMakeFiles/pio.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pio.dir/pio.o"
+  "pio.pdb"
+  "pio"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/pio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
